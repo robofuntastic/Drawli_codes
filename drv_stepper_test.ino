d1e355b0be870c560bd84b26309fc282e0f2f044
RoboFunstastic <142223311+robofuntastic@users.noreply.github.com>
2024-02-08 18:01:38 +0500

    Update drv_stepper_test.ino

--- drv_stepper_test.ino
+++ drv_stepper_test.ino
@@ -1,10 +1,11 @@
+#include <Arduino.h>
 
 #include <AccelStepper.h>
 #include <cmath>
 
 #define enable_pin 5
-const float wheel_separation = 160.0;  
-const float wheel_radius = 37.0;  
+const float wheel_separation = 140.0;  
+const float wheel_radius = 35.0;  
 
 int angMotorSpeedLeftStep = 0;
 int angMotorSpeedRightStep = 0;
@@ -19,8 +20,8 @@ AccelStepper left_stepper = AccelStepper(motorInterfaceType, left_stepPin, left_
 void setup()
 {  
   //Serial.begin(115200); 
-   right_stepper.setMaxSpeed(1600); //Steps per second
-   left_stepper.setMaxSpeed(1600);
+   right_stepper.setMaxSpeed(6000); //Steps per second
+   left_stepper.setMaxSpeed(6000);
 
    
    pinMode(enable_pin, OUTPUT);
@@ -84,38 +85,40 @@ bool Rotate(int Speed,int Degrees, int Dir) {
   return true;
 }
 
-
+// mm/s , mm, degree
 bool Circle(int Speed, int Diameter,int Degrees, int Dir) {
-   int Angular_speed = Speed/(Diameter/2);
-   calcSpeed(Speed, Angular_speed);
-   float desired_angle_rad = Degrees * 3.14/180;
-   float wheel_angle = wheel_separation *  desired_angle_rad/ ( 2 * wheel_radius); // radians
-   int wheel_angle_steps = wheel_angle * 1600 / (2 * 3.14);
+  float circle_radius = Diameter/2;
+  float Angular_speed = Speed/circle_radius;  //rad
+  calcSpeed(Speed, Angular_speed);
+
+  float left_wheel_angle = (Diameter/2 - wheel_separation/2) * (Degrees*3.14/180)/wheel_radius;
+  int left_wheel_steps  = left_wheel_angle * 1600 / (2 * 3.14);
    
-  right_stepper.move(-wheel_angle_steps * Dir); //- for making it right direction
+  float right_wheel_angle = (Diameter/2 + wheel_separation/2) * (Degrees*3.14/180)/wheel_radius;
+  int right_wheel_steps  = right_wheel_angle * 1600 / (2 * 3.14);
+
+
+  right_stepper.move(-right_wheel_steps * Dir); //- for making it right direction
   right_stepper.setSpeed(angMotorSpeedRightStep);
 
-  left_stepper.move(-wheel_angle_steps * Dir);
+  left_stepper.move(left_wheel_steps * Dir);
   left_stepper.setSpeed(angMotorSpeedLeftStep);
   
 
   while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0) {
     right_stepper.runSpeedToPosition();
     left_stepper.runSpeedToPosition();
+    //right_stepper.runSpeed();
+    //left_stepper.runSpeed();
   }
-  
+
   return true;
 }
 
 void loop()
 {  
 
-  Straight(150,1000,1); //units mm/s and mm
-  delay(2000);
-  Rotate(15,175,1);
-  delay(2000);
-  Straight(50,1000,1);
-  delay(2000);
-  Rotate(30,-175,1);
-  delay(2000);
+Circle(100, 300, 90, 1);
+delay(5000);
+
 }
