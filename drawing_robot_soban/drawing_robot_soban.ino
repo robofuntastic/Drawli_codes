9aada6ef0a1af45aa4ea81b7293f037b01d189e9
sobanahmed21 <sa.sobanahmed@gmail.com>
2024-04-07 15:24:16 +0500

    resolved bug

--- drawing_robot_soban/drawing_robot_soban.ino
+++ drawing_robot_soban/drawing_robot_soban.ino
@@ -22,8 +22,8 @@ const float wheel_radius = 35.5;
 int angMotorSpeedLeftStep = 0;
 int angMotorSpeedRightStep = 0;
 
-#define CCW true // default turn direction
-#define CW false // opposite turn direction
+#define CCW true  // default turn direction
+#define CW false  // opposite turn direction
 
 #define right_stepPin 12
 #define right_dirPin 13
@@ -35,8 +35,7 @@ AccelStepper left_stepper = AccelStepper(motorInterfaceType, left_stepPin, left_
 
 // Function to calculate motor speeds based on robot linear and angular velocities
 // units mm/s and rad/s
-void calcSpeed(float robotLinear, float robotAngular)
-{
+void calcSpeed(float robotLinear, float robotAngular) {
   // Convert linear and angular velocities to rad/s
   float angMotorSpeedLeft = (robotLinear - robotAngular * wheel_separation / 2) / wheel_radius;
   float angMotorSpeedRight = (robotLinear + robotAngular * wheel_separation / 2) / wheel_radius;
@@ -47,8 +46,7 @@ void calcSpeed(float robotLinear, float robotAngular)
 }
 
 // speed in mm/s
-bool Straight(int Speed, float Distance, int Dir)
-{
+bool Straight(int Speed, float Distance, int Dir) {
 
   calcSpeed(Speed, 0);
   // Serial.println(angMotorSpeedLeftStep, angMotorSpeedRightStep);
@@ -56,14 +54,13 @@ bool Straight(int Speed, float Distance, int Dir)
   // Set speed and direction for both steppers
   int dis = Distance * 1600 / (PI * wheel_radius * 2);
 
-  right_stepper.move(-dis * Dir); //- for making it right direction
+  right_stepper.move(-dis * Dir);  //- for making it right direction
   right_stepper.setSpeed(angMotorSpeedRightStep);
 
   left_stepper.move(dis * Dir);
   left_stepper.setSpeed(angMotorSpeedLeftStep);
 
-  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0)
-  {
+  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0) {
     right_stepper.runSpeedToPosition();
     left_stepper.runSpeedToPosition();
   }
@@ -71,22 +68,20 @@ bool Straight(int Speed, float Distance, int Dir)
 }
 
 // speed degrees/s
-bool Rotate(int Speed, int Degrees, int Dir)
-{
-  calcSpeed(0, Speed * PI / 180); // Converting deg/s to rad/s
+bool Rotate(int Speed, int Degrees, int Dir) {
+  calcSpeed(0, Speed * PI / 180);  // Converting deg/s to rad/s
 
   float desired_angle_rad = Degrees * PI / 180;
-  float wheel_angle = wheel_separation * desired_angle_rad / (2 * wheel_radius); // radians
+  float wheel_angle = wheel_separation * desired_angle_rad / (2 * wheel_radius);  // radians
   int wheel_angle_steps = wheel_angle * 1600 / (2 * PI);
 
-  right_stepper.move(-wheel_angle_steps * Dir); //- for making it right direction
+  right_stepper.move(-wheel_angle_steps * Dir);  //- for making it right direction
   right_stepper.setSpeed(angMotorSpeedRightStep);
 
   left_stepper.move(-wheel_angle_steps * Dir);
   left_stepper.setSpeed(angMotorSpeedLeftStep);
 
-  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0)
-  {
+  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0) {
     right_stepper.runSpeedToPosition();
     left_stepper.runSpeedToPosition();
   }
@@ -95,10 +90,9 @@ bool Rotate(int Speed, int Degrees, int Dir)
 }
 
 // mm/s , mm, degree
-bool Circle(int Speed, int Diameter, int Degrees, int f_b, int Dir)
-{
+bool Circle(int Speed, int Diameter, int Degrees, int f_b, int Dir) {
   float circle_radius = Diameter / 2;
-  float Angular_speed = Speed / circle_radius; // rad
+  float Angular_speed = Speed / circle_radius;  // rad
   calcSpeed(Speed, Angular_speed);
 
   float left_wheel_angle = (Diameter / 2 - wheel_separation / 2) * (Degrees * PI / 180) / wheel_radius;
@@ -107,25 +101,22 @@ bool Circle(int Speed, int Diameter, int Degrees, int f_b, int Dir)
   float right_wheel_angle = (Diameter / 2 + wheel_separation / 2) * (Degrees * PI / 180) / wheel_radius;
   int right_wheel_steps = right_wheel_angle * 1600 / (2 * PI);
 
-  if (Dir == 1) // cw direction
+  if (Dir == 1)  // cw direction
   {
-    right_stepper.move(-right_wheel_steps * f_b); //- for making it right direction
+    right_stepper.move(-right_wheel_steps * f_b);  //- for making it right direction
     right_stepper.setSpeed(angMotorSpeedRightStep);
 
     left_stepper.move(left_wheel_steps * f_b);
     left_stepper.setSpeed(angMotorSpeedLeftStep);
-  }
-  else
-  {                                              // ccw direction
-    right_stepper.move(-left_wheel_steps * f_b); //- for making it right direction
+  } else {                                        // ccw direction
+    right_stepper.move(-left_wheel_steps * f_b);  //- for making it right direction
     right_stepper.setSpeed(angMotorSpeedLeftStep);
 
     left_stepper.move(right_wheel_steps * f_b);
     left_stepper.setSpeed(angMotorSpeedRightStep);
   }
 
-  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0)
-  {
+  while (right_stepper.distanceToGo() != 0 || left_stepper.distanceToGo() != 0) {
     right_stepper.runSpeedToPosition();
     left_stepper.runSpeedToPosition();
   }
@@ -133,8 +124,7 @@ bool Circle(int Speed, int Diameter, int Degrees, int f_b, int Dir)
   return true;
 }
 
-void move(float Distance)
-{
+void move(float Distance) {
   Straight(100, Distance, 1);
 }
 
@@ -144,19 +134,16 @@ void move(float Distance)
  Note ... since the motors are both inwards pointing the wheels require
  the same patterns if they are to rotate in opposite directions.
  ***************************************************************************/
-void rotate(float angle, bool turn_ccw)
-{
+void rotate(float angle, bool turn_ccw) {
   int dir = 1;
   //--------------------
   // take smallest turn
   //--------------------
-  if (angle > PI)
-  { // is the interior angle smaller?
+  if (angle > PI) {  // is the interior angle smaller?
     angle = 2 * PI - angle;
     turn_ccw = !turn_ccw;
   }
-  if (turn_ccw == false)
-  {
+  if (turn_ccw == false) {
     dir = -1;
   }
   Rotate(25, angle * 180 / PI, dir);
@@ -168,97 +155,87 @@ void rotate(float angle, bool turn_ccw)
  a bearing (radians) from its current co-ordinate. The robot always aligns
  itself with the new bearing before moving.
  ***************************************************************************/
-void move_to(float x2, float y2)
-{
+void move_to(float x2, float y2) {
 
   //----------------------------------------------------------
   // static values (contents remain between function calls)
   //----------------------------------------------------------
-  static float x1, y1 = 0;      // intial co-ordinates
-  static float old_bearing = 0; // current robot bearing from 3 o'clock
+  static float x1, y1 = 0;       // intial co-ordinates
+  static float old_bearing = 0;  // current robot bearing from 3 o'clock
 
   //----------------------------
   // calculate distance (steps)
   //----------------------------
   float dx = x2 - x1;
   float dy = y2 - y1;
-  float distance = sqrt(dx * dx + dy * dy); // steps (pythagoras)
+  float distance = sqrt(dx * dx + dy * dy);  // steps (pythagoras)
 
   //----------------------------------
   // calculate true bearing (radians)
   //----------------------------------
   int quadrant;
-  float new_bearing; // new bearing
+  float new_bearing;  // new bearing
 
-  if ((dx == 0) & (dy == 0))
-  {
+  if ((dx == 0) & (dy == 0)) {
     quadrant = 0;
-  } // no change
-  if ((dx > 0) & (dy >= 0))
-  {
+  }  // no change
+  if ((dx > 0) & (dy >= 0)) {
     quadrant = 1;
   }
-  if ((dx <= 0) & (dy > 0))
-  {
+  if ((dx <= 0) & (dy > 0)) {
     quadrant = 2;
   }
-  if ((dx < 0) & (dy <= 0))
-  {
+  if ((dx < 0) & (dy <= 0)) {
     quadrant = 3;
   }
-  if ((dx >= 0) & (dy < 0))
-  {
+  if ((dx >= 0) & (dy < 0)) {
     quadrant = 4;
   }
-  switch (quadrant)
-  {
-  case 0:
-  {
-    new_bearing = 0;
-    break;
-  }
-  case 1:
-  {
-    new_bearing = 0 + asin(dy / distance);
-    break;
-  }
-  case 2:
-  {
-    new_bearing = PI / 2 + asin(-dx / distance);
-    break;
-  }
-  case 3:
-  {
-    new_bearing = PI + asin(-dy / distance);
-    break;
-  }
-  case 4:
-  {
-    new_bearing = 2 * PI - asin(-dy / distance);
-    break;
-  }
-  default:
-  {
-    break;
-  }
+  switch (quadrant) {
+    case 0:
+      {
+        new_bearing = 0;
+        break;
+      }
+    case 1:
+      {
+        new_bearing = 0 + asin(dy / distance);
+        break;
+      }
+    case 2:
+      {
+        new_bearing = PI / 2 + asin(-dx / distance);
+        break;
+      }
+    case 3:
+      {
+        new_bearing = PI + asin(-dy / distance);
+        break;
+      }
+    case 4:
+      {
+        new_bearing = 2 * PI - asin(-dy / distance);
+        break;
+      }
+    default:
+      {
+        break;
+      }
   }
 
   //----------------------------------------------------------
   // align robot with next bearing.
   //----------------------------------------------------------
-  if (new_bearing < old_bearing)
-  {
+  if (new_bearing < old_bearing) {
     rotate(old_bearing - new_bearing, CW);
-  }
-  else
-  {
+  } else {
     rotate(new_bearing - old_bearing, CCW);
   }
 
   //------------------------
   // move robot along axis
   //------------------------
-  move(distance); // move the robot
+  move(distance);  // move the robot
 
   //------------------------
   // update the static values
@@ -268,8 +245,7 @@ void move_to(float x2, float y2)
   old_bearing = new_bearing;
 }
 
-void remote_control(float lin, float ang)
-{
+void remote_control(float lin, float ang) {
   calcSpeed(lin * 1000, -ang);
   left_stepper.setSpeed(angMotorSpeedLeftStep);
   right_stepper.setSpeed(-angMotorSpeedRightStep);
@@ -282,8 +258,7 @@ void remote_control(float lin, float ang)
   // Serial.println(angMotorSpeedRightStep);
 }
 
-void command_handler(String line)
-{
+void command_handler(String line) {
   // Extract command and arguments
   int index = line.indexOf(',');
   String command = line.substring(0, index);
@@ -304,8 +279,7 @@ void command_handler(String line)
   Serial.println("Arg2: " + arg2);
 
   // Check if the command is "rc" and call remote_control if it is
-  if (command == "rc")
-  {
+  if (command == "rc") {
     remote_control(arg1.toFloat(), arg2.toFloat());
   }
 }
@@ -313,34 +287,27 @@ void command_handler(String line)
 SemaphoreHandle_t xSemaphoreActivateParrallelTasks;
 
 // Task function for running parrallel
-void parrallel_tasks(void *pvParameters)
-{
-  while (true)
-  {
+void parrallel_tasks(void *pvParameters) {
+  while (true) {
     xSemaphoreTake(xSemaphoreActivateParrallelTasks, portMAX_DELAY);
     command_handler(receivedLine);
     // vTaskDelay(1 / portTICK_PERIOD_MS);  // Delay for 1
   }
 }
 
-void receive_cmds(void *ptr)
-{
+void receive_cmds(void *ptr) {
 
   // Start server
   server.begin();
   Serial.println("HTTP server started");
 
-  while (1)
-  {
+  while (1) {
     server.handleClient();
     WiFiClient client = server.client();
-    if (client.connected())
-    {
+    if (client.connected()) {
       Serial.println("Client connected");
-      while (client.connected())
-      {
-        if (client.available())
-        {
+      while (client.connected()) {
+        if (client.available()) {
           receivedLine = client.readStringUntil('\r');
           // Serial.print("Received from client: ");
           // Serial.println(receivedLine);
@@ -350,6 +317,7 @@ void receive_cmds(void *ptr)
           // client.println(line);
           xSemaphoreGive(xSemaphoreActivateParrallelTasks);
         }
+        vTaskDelay(1);
       }
       client.stop();
       Serial.println("Client disconnected");
@@ -361,8 +329,7 @@ void receive_cmds(void *ptr)
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   /* Attempt to create a semaphore. */
   xSemaphoreActivateParrallelTasks = xSemaphoreCreateBinary();
@@ -381,26 +348,25 @@ void setup()
 
   // Create a task to run the Straight function
   xTaskCreatePinnedToCore(
-      receive_cmds,
-      "receive_cmds",
-      1024 * 10, // Stack size
-      NULL,
-      1, // Priority
-      NULL,
-      0 // Core to run the task on (Core 0)
+    receive_cmds,
+    "receive_cmds",
+    1024 * 10,  // Stack size
+    NULL,
+    1,  // Priority
+    NULL,
+    0  // Core to run the task on (Core 0)
   );
 
 
-xTaskCreatePinnedToCore(
+  xTaskCreatePinnedToCore(
     parrallel_tasks,
     "parrallel_tasks",
-    10000, // Stack size
+    10000,  // Stack size
     NULL,
-    1, // Priority
+    1,  // Priority
     NULL,
-    1 // Core to run the task on (Core 0)
-);
+    1  // Core to run the task on (Core 0)
+  );
 }
-void loop()
-{
+void loop() {
 }
