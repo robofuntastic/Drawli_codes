7a0da6eb6313edbf6efb998fee5eadb1309d26f6
RoboFunstastic <142223311+robofuntastic@users.noreply.github.com>
2024-04-24 01:59:35 +0500

    Update socket_connection_testing.ino

--- socket_connection_testing.ino
+++ socket_connection_testing.ino
@@ -1,52 +1,37 @@
 #include <WiFi.h>
-#include <WebServer.h>
 
 const char *ssid = "RoboFuntastic";
 const char *password = "123456789";
-
-WebServer server(80);
-
-void handleRoot() {
-  server.send(200, "text/plain", "hello to you");
-}
-
-void handleButtonPress() {
-  server.send(200, "text/plain", "hi from web");
-}
+WiFiServer server(80);
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  WiFi.softAP(ssid, password, 1, 0);
+  delay(500);
+  Serial.println("Configuring access point...");
+  WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-
-  // Route setup
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/button", HTTP_POST, handleButtonPress);
-
-  // Start server
   server.begin();
-  Serial.println("HTTP server started");
+  
 }
 
 void loop() {
-  server.handleClient();
-  WiFiClient client = server.client();
-  if (client.connected()) {
-    Serial.println("Client connected");
-    while (client.connected()) {
-      if (client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print("Received from client: ");
-        Serial.println(line);
-        client.println("Received from client: ");
-        client.println(line);
-      }
+ WiFiClient client = server.available();   // listen for incoming clients
+
+
+  if (client) {
+    while (client.connected()) { 
+    if (client.available()) {
+      String command = client.readStringUntil('\r');
+      Serial.println("Received command: " + command);
+      // Process the command here (e.g., move motors based on the received command)
     }
-    client.stop();
-    Serial.println("Client disconnected");
+  } 
   }
+
 }
+
+
+//ESP32 
